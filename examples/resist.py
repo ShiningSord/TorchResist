88a5efa744c9b23b9dc35096179a913081ef74f1
ShiningSord <zixiaowang97@gmail.com>
2024-10-07 20:21:58 +0800

    fix examples

--- examples/resist.py
+++ examples/resist.py
@@ -6,8 +6,8 @@ from PIL import Image
 
 
 simulator = get_default_simulator()
-    
 aerial_image = Image.open('examples/aerial.png')
 aerial_image = torch.from_numpy(np.array(aerial_image))/255.0
+aerial_image = torch.cat([aerial_image.T[None],aerial_image[None]],0).to(simulator.device)
 # import pdb; pdb.set_trace()
 res = simulator.forward(aerial_image, dx=7.0)
